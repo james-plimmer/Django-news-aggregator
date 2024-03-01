c3cce8f4cc4ac859235b2f70ee51b3699d62cdfb
James Plimmer <sc21jpwp@leeds.ac.uk>
2024-03-01 12:08:39 +0000

    Added models, migrated and registered to admin site

--- cwk1/news/models.py
+++ cwk1/news/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Story(models.Model):
+    headline = models.CharField(max_length=64)
+    category = models.CharField(max_length=32, choices=[('pol', 'Politics'), ('art', 'Art'), ('tech', 'Technology'), ('trivia', 'Trivia')])
+    region = models.CharField(max_length=32, choices=[('uk', 'UK'), ('eu', 'European'), ('w', 'World')])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
+    details = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.title
