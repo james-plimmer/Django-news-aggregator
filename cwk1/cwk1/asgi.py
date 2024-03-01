d44063dec4f93b6da2cf3d82c749aad40aa52578
James Plimmer <sc21jpwp@leeds.ac.uk>
2024-03-01 11:49:00 +0000

    Corrected project setup

--- cwk1/cwk1/asgi.py
+++ cwk1/cwk1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for news_aggregator project.
+ASGI config for cwk1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news_aggregator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cwk1.settings')
 
 application = get_asgi_application()
--- cwk1/cwk1/urls.py
+++ cwk1/cwk1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for news_aggregator project.
+URL configuration for cwk1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
